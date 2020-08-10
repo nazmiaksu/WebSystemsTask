4b9ea36ccd8db92852dd0b9569035436f3a1abb4
nazmiaksu <62637219+nazmiaksu@users.noreply.github.com>
2020-08-10 03:34:51 +0100

    Web Systems

---
+++ backend/1920543/1920543nazmi/1920543nazmi/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1920543nazmi.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cappadocia()
+        {
+            return View();
+        }
+        public ActionResult History()
+        {
+         
+            return View();
+        }
+        public ActionResult TouristAttraction()
+        {
+           
+            return View();
+        }
+        public ActionResult Visa()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult RentACar()
+        {
+            return View();
+        }
+        public ActionResult Hotel()
+        {
+            return View();
+        }
+        public ActionResult Airport()
+        {
+            return View();
+        }
+    }
+}
---
+++ backend/1920543/1920543nazmi/1920543nazmi/Controllers/PartialController.cs
@@ -0,0 +1,40 @@
+﻿using _1920543nazmi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1920543nazmi.Controllers
+{
+   
+    public class PartialController : Controller
+    {
+        nazmidbEntities db = new nazmidbEntities();
+        // GET: Partial
+        public PartialViewResult ContentText()
+        {
+            return PartialView(db.CappadociaContents.ToList());
+        }
+        public PartialViewResult ContentText2()
+        {
+            return PartialView(db.CappadociaContents.ToList());
+        }
+      
+        public PartialViewResult Slider()
+        {
+            return PartialView(db.Sliders.ToList());
+        }
+        public PartialViewResult Picture()
+        {
+            
+            return PartialView(db.Pictures.ToList());
+        }
+
+
+
+
+
+
+    }
+}
